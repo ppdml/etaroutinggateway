87861c6b24b0fc21a0e0e11f5d974f9d673f1175
Timon Sachweh <timon@sachweh.de>
2024-11-04 22:18:16 +0100

    Fix geo coordinates for demo scenario in osnabrueck

--- src/main/kotlin/de/tudo/etaroutinggateway/controllers/SampleController.kt
+++ src/main/kotlin/de/tudo/etaroutinggateway/controllers/SampleController.kt
@@ -2,22 +2,28 @@ package de.tudo.etaroutinggateway.controllers
 
 import de.tudo.etaroutinggateway.entities.dtos.gaiax.RoutingRequestDto
 import de.tudo.etaroutinggateway.entities.dtos.gaiax.RoutingResponseDto
+import de.tudo.etaroutinggateway.services.OtpRouteMappingService
 import de.tudo.etaroutinggateway.services.RoutingRequestGeneratorService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/sample")
 class SampleController(
     @Autowired
-    val routingRequestGeneratorService: RoutingRequestGeneratorService
+    val routingRequestGeneratorService: RoutingRequestGeneratorService,
+    @Autowired
+    val otpRouteMappingService: OtpRouteMappingService
 ) {
 
     @GetMapping("/generate")
     fun generateRandomRequest(): RoutingRequestDto {
         return routingRequestGeneratorService.generateAndSendKafkaRoutingRequest()
+    }
+
+    @PostMapping("/routing")
+    fun getResponseForRequest(@RequestBody request: RoutingRequestDto): RoutingResponseDto {
+        return otpRouteMappingService.getRouteForRequest(request)
     }
 
     @GetMapping("/otpsamplerequest")
--- src/main/kotlin/de/tudo/etaroutinggateway/services/OtpRouteMappingService.kt
+++ src/main/kotlin/de/tudo/etaroutinggateway/services/OtpRouteMappingService.kt
@@ -23,30 +23,72 @@ class OtpRouteMappingService(
         if (routingRequest.routeLocations.size < 2) throw Exception("Two locations are required for routing")
         var responses = ArrayList<RoutingFeatureDto>()
         for (i in 0..routingRequest.routeLocations.size - 2) {
-            responses.addAll(this.getGaiaXRouteForStartAndEnd(routingRequest.routeLocations[i],
-                routingRequest.routeLocations[i + 1],
-                routingRequest.metadata.vehicleType))
+            responses.addAll(
+                this.getGaiaXRouteForStartAndEnd(
+                    routingRequest.routeLocations[i],
+                    routingRequest.routeLocations[i + 1],
+                    routingRequest.metadata.vehicleType
+                )
+            )
         }
-        return RoutingResponseDto(features = responses,)
+
+        return RoutingResponseDto(features = responses)
     }
 
-    fun getGaiaXRouteForStartAndEnd(start: RouteLocationDto, end: RouteLocationDto, mode: VehicleType): List<RoutingFeatureDto> {
+    fun getGaiaXRouteForStartAndEnd(
+        start: RouteLocationDto,
+        end: RouteLocationDto,
+        mode: VehicleType
+    ): List<RoutingFeatureDto> {
         val response = this.getRouteForStartEnd(start, end, mode)
         val routingResponseKafka = response.toGaiaxRoutingResponseDto()
+
+        var updatedList: List<List<Double>> = listOf()
+        if (routingResponseKafka.features.isNotEmpty()) {
+            updatedList = routingResponseKafka.features[0].geometry.coordinates
+        }
+        if (routingResponseKafka.features.isEmpty() ||
+            routingResponseKafka.features[0].geometry.coordinates.isEmpty() ||
+            (52.310731857708134 < start.latitude &&  start.latitude < 52.317208653922705 &&
+             8.003401925933604 < start.longitude && start.longitude < 8.012513791827066)
+        ) {
+            updatedList = listOf(listOf(start.longitude, start.latitude)) + updatedList
+        }
+
+        if (routingResponseKafka.features.isEmpty() ||
+            routingResponseKafka.features[0].geometry.coordinates.isEmpty() ||
+            (52.310731857708134 < end.latitude &&  end.latitude < 52.317208653922705 &&
+                    8.003401925933604 < end.longitude && end.longitude < 8.012513791827066)
+        ) {
+            updatedList = updatedList + listOf(listOf(end.longitude, end.latitude))
+        }
+
+        if (routingResponseKafka.features.isEmpty()) {
+            routingResponseKafka.features = listOf(
+                RoutingFeatureDto(
+                    geometry = RoutingGeometryDto(coordinates = updatedList),
+                )
+            )
+        } else {
+            routingResponseKafka.features[0].geometry.coordinates = updatedList
+        }
         return routingResponseKafka.features
     }
 
     fun handleRouteRequestAndSendResponse(routingRequestDto: RoutingRequestDto?) {
-        kafkaService.sendRoutingResponse(getRouteForRequest(routingRequestDto!!),
+        kafkaService.sendRoutingResponse(
+            getRouteForRequest(routingRequestDto!!),
             routingRequestDto.requestId.toString()
         )
     }
 
     fun getRouteForStartEnd(start: RouteLocationDto, end: RouteLocationDto, mode: VehicleType): OtpResponseDto {
         val requestUrl = "/otp/routers/default/plan?fromPlace=${start.latitude},${start.longitude}" +
-                "&toPlace=${end.latitude},${end.longitude}&time=4:12pm&date=01-23-2024&mode=$mode&arriveBy=false" +
+                "&toPlace=${end.latitude},${end.longitude}&time=4:12pm&date=01-23-2024&mode=CAR&arriveBy=false" +
                 "&wheelchair=false&showIntermediateStops=true&locale=en"
-        val routingResponse: OtpResponseDto? = restTemplate.getForObject(otpServerAddress + requestUrl, OtpResponseDto::class.java)
-        return routingResponse!!
+        val routingResponse: OtpResponseDto =
+            restTemplate.getForObject(otpServerAddress + requestUrl, OtpResponseDto::class.java)
+                ?: throw IllegalStateException("Could not find OtpResponseDto")
+        return routingResponse
     }
 }
